762d14f8743e8e5a0cba7929d6f8d26ccc3b55d8
freewind <nowindlee@gmail.com>
2018-04-01 20:07:08 +0800

    method override

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-Python3 Hello World Demo
-=========================
+Python3 Method Overridden Demo
+==============================
+
+It's a bit different from Java.
+
+You can call any other class's super methods.
 
 ```
 python3 hello.py
--- hello.py
+++ hello.py
@@ -1,2 +1,18 @@
-if __name__ == "__main__":
-  print("Hello, Python!")
+class Parent:
+    def hello(self):
+        print("Hello from Parent")
+
+class Child(Parent):
+    def hello(self):
+        print("Hello from Child")
+
+class GrandChild(Child):
+    def hello(self):
+        print("Hello from GrandChild")
+        print("---------- call Child.hello() ---------")
+        super(GrandChild, self).hello()
+        print("---------- call Parent.hello() ---------")
+        super(Child, self).hello()
+
+grand_child = GrandChild()
+grand_child.hello()
